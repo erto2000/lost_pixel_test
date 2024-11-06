dd297d659e14558ddcebdd040a1f911b1ae90925
erto2000 <ertugrulkeskin2000@hotmail.com>
2024-11-06 19:43:23 +0300

    files added

---
+++ lostpixel.config.js
@@ -0,0 +1,8 @@
+module.exports = {
+      pageShots: {
+      pages: [ { path: '/', name: 'landing' } ],
+      baseUrl: 'http://172.17.0.1:3000',
+    },
+      lostPixelProjectId: 'cm363gq9l0k6nqwvybegpzlb8',
+      apiKey: process.env.LOST_PIXEL_API_KEY,
+    };
