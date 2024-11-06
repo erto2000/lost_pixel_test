b5feafc6afc55ea2c46e8d22b8f42f91e5314183
erto2000 <ertugrulkeskin2000@hotmail.com>
2024-11-06 21:23:55 +0300

    last commit

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -1,16 +1,8 @@
 module.exports = {
-  pageShots: {
-    pages: [
-      {
-        path: '/',
-        name: 'landing',
-        beforeScreenshot: async (page) => {
-          await page.click('button'); // Adjust this selector if necessary
-        },
-      },
-    ],
-    baseUrl: 'http://172.17.0.1:3000',
-  },
-  lostPixelProjectId: 'cm363gq9l0k6nqwvybegpzlb8',
-  apiKey: process.env.LOST_PIXEL_API_KEY,
-};
+      pageShots: {
+      pages: [ { path: '/', name: 'landing' } ],
+      baseUrl: 'http://172.17.0.1:3000',
+    },
+      lostPixelProjectId: 'cm363gq9l0k6nqwvybegpzlb8',
+      apiKey: process.env.LOST_PIXEL_API_KEY,
+    };
