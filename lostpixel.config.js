d8aae40a439f6f1c7975e3be69a52e2dd0703faf
erto2000 <ertugrulkeskin2000@hotmail.com>
2024-11-06 21:13:33 +0300

    adf

--- lostpixel.config.js
+++ lostpixel.config.js
@@ -1,13 +1,8 @@
 module.exports = {
-  pageShots: {
-    pages: [{ path: '/', name: 'landing' }],
-    baseUrl: 'http://172.17.0.1:3000',
-    breakpoints: [375, 768, 1024], // Adding responsive breakpoints
-    mask: [{ selector: '.dynamic-item' }], // Corrected to an object with selector
-  },
-  lostPixelProjectId: 'cm363gq9l0k6nqwvybegpzlb8',
-  apiKey: process.env.LOST_PIXEL_API_KEY,
-  browsers: ['chromium', 'firefox'], // Adjusted key and values to supported options
-  threshold: 0.05, // Allow slight differences in pixels for minor variations
-  waitForSelector: '.loading', // Optional: Wait for loading spinner to disappear if needed
-};
+      pageShots: {
+      pages: [ { path: '/', name: 'landing' } ],
+      baseUrl: 'http://172.17.0.1:3000',
+    },
+      lostPixelProjectId: 'cm363gq9l0k6nqwvybegpzlb8',
+      apiKey: process.env.LOST_PIXEL_API_KEY,
+    };
